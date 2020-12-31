976fa6fffe98e9eb5af5fbbe03d4ed1e4d5c7925
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2020-12-31 14:42:20 +0000

    Roman Numerals TTD Passed 1st Test

---
+++ basic_kata/Roman_Numerals/lib/roman_numerals.rb
@@ -0,0 +1,26 @@
+# It can be tricky to translate numbers into their roman numeral equivalent, write a program that takes a number between 1 and 1000 and returns the corresponding roman numeral.
+#
+# Brief guide to roman numerals:
+# 1 = I
+# 5 = V
+# 10 = X
+# 50 = L
+# 100 = C
+# 1000 = M
+class RomanNumerals
+
+  # VALUES = {
+  #   "M" => 1000,
+  #   "D" => 500,
+  #   "C" => 100,
+  #   "L" => 50,
+  #   "X" => 10,
+  #   "V" => 5,
+  #   "I" => 1
+  # }
+
+  def self.convert(number)
+    "I"
+  end
+
+end
